706058beb2571da2ac48897a3d26c85d593ef0bd
arehmankhan613-create <arehmankhan613@gmail.com>
2026-08-10 16:33:38 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,1 +1,44 @@
+// ==========================================
+// MediCore
+// Database Connection
+// ==========================================
 
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+async function testDatabaseConnection() {
+
+    try {
+
+        const connection = await pool.getConnection();
+
+        console.log("✅ MySQL Database Connected Successfully");
+
+        connection.release();
+
+    } catch (error) {
+
+        console.error(
+            "❌ Database Connection Failed:",
+            error.message
+        );
+
+    }
+
+}
+
+testDatabaseConnection();
+
+module.exports = pool;
